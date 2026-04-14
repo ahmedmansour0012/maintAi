0bb1a7972e16c72303074f0377163ec704af3500
nada-mah <cutenada.lara4@gmail.com>
2026-04-14 22:10:25 +0200

    modified:   Api/seed_sqlite.py

--- Api/seed_sqlite.py
+++ Api/seed_sqlite.py
@@ -36,7 +36,7 @@ try:
     # -------------------------
     contact1 = CustomerServiceContact(
         subcontractor_id=rinnai.id,
-        phone_number_e164="+18006219415",
+        phone_number_e164="+19707363821",
         email="support@rinnai.us",
         operating_hours="Mon-Fri 8am-5pm",
         language="en",
